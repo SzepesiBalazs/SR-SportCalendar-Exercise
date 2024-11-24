57b96dae09913172cdb8d8381a2a7ac418333013
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2024-11-24 15:17:40 +0100

    Changes hardcoded date values to dynamic date values

--- src/views/EventCalendar.vue
+++ src/views/EventCalendar.vue
@@ -2,18 +2,34 @@
   <div
     class="box-content h-32 mb-8 border-4 place-content-center text-center bg-blue-200 rounded-md"
   >
-    <p class="text-2xl font-extrabold">2024. November</p>
+    <p class="text-2xl font-extrabold">{{ currentMonth }}</p>
   </div>
   <div class="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-1 place-items-center">
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
-    <div class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4">01</div>
+    <div
+      v-for="day in numberOfDays"
+      :key="day"
+      class="box-content md:h-32 md:w-32 h-64 w-64 p-4 m-2 border-4"
+    >
+      {{ day }}
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { ref, computed } from 'vue'
+export default {
+  setup() {
+    const currentMonth = computed(() =>
+      new Date().toLocaleString('default', { year: 'numeric', month: 'long' }),
+    )
+    const numberOfDays = computed(() => {
+      const date = new Date()
+      const currentMonth = date.getMonth()
+      const currentYear = date.getFullYear()
+      const nextMonth = new Date(currentYear, currentMonth + 1, 0)
+      return nextMonth.getDate()
+    })
+    return { currentMonth, numberOfDays }
+  },
+}
+</script>
