3921f77ea384bdc16a45bd45c001dc3250e4aced
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2024-11-24 17:51:34 +0100

    Adds Sportdata.json and implement it in the Event Calendar

--- src/views/EventCalendar.vue
+++ src/views/EventCalendar.vue
@@ -6,31 +6,68 @@
   </div>
   <div class="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-1 place-items-center">
     <div
-      v-for="day in numberOfDays"
-      :key="day"
+      v-for="dayObject in daysArray"
+      :key="dayObject.day"
       class="box-content md:h-48 md:w-48 lg:h-64 lg:w-64 sm:h-64 sm:w-64 p-4 m-2 border-4 grid grid-cols-3"
     >
-      <p class="col-span-1">{{ day }}</p>
-      <p class="col-span-2">18:30, Football, Salzburg vs. Sturm</p>
+      <div class="col-span-1">
+        <p>{{ dayObject.day }}</p>
+      </div>
+      <div class="col-span-2">
+        <div v-for="(event, index) in dayObject.events" :key="index">
+          <p>
+            {{ validateCompetitor(event.homeTeam) }} vs {{ validateCompetitor(event.awayTeam) }}
+          </p>
+          <p>{{ formatingUTCTime(event.timeVenueUTC) }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import sportData from '../sportData.json'
+import dayjs from 'dayjs'
 export default {
   setup() {
+    const calendarData = ref(sportData)
+    const daysArray = ref([])
     const currentMonth = computed(() =>
       new Date().toLocaleString('default', { year: 'numeric', month: 'long' }),
     )
-    const numberOfDays = computed(() => {
-      const date = new Date()
-      const currentMonth = date.getMonth()
-      const currentYear = date.getFullYear()
-      const nextMonth = new Date(currentYear, currentMonth + 1, 0)
-      return nextMonth.getDate()
+
+    const generateDaysOfMonths = () => {
+      const currentDate = dayjs()
+
+      const daysInMonth = currentDate.daysInMonth()
+
+      for (let day = 1; day <= daysInMonth; day++) {
+        const date = dayjs().date(day).format('YYYY-MM-DD')
+        console.log(calendarData.value.data)
+        const events = calendarData.value.data.filter((data) => data.dateVenue === date)
+
+        daysArray.value.push({
+          day: day.toString().padStart(2, '0'),
+          date: date,
+          events: events,
+        })
+      }
+    }
+
+    onMounted(() => {
+      generateDaysOfMonths()
     })
-    return { currentMonth, numberOfDays }
+
+    const validateCompetitor = (team) => {
+      return team?.officialName ?? 'Unknown'
+    }
+
+    const formatingUTCTime = (time) => {
+      return time.slice(0, -3)
+    }
+
+    return { currentMonth, daysArray, validateCompetitor, formatingUTCTime }
   },
 }
 </script>
