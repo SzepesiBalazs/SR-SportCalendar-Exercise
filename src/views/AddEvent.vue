1105bcb95a6a23d3d34cd64d52329d378c3db09d
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2024-11-28 12:50:46 +0100

    Adds forms for add event page

--- src/views/AddEvent.vue
+++ src/views/AddEvent.vue
@@ -1,5 +1,142 @@
 <template>
-  <div class="add-event">
-    <h1>This is an about page</h1>
-  </div>
+  <form>
+    <div class="space-y-12">
+      <div class="pb-12">
+        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div class="sm:col-span-4">
+            <label for="username" class="block text-sm/6 font-medium text-gray-900"></label>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Select date"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Select start time"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Select start time"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Add home country"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Add home competitor"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Add away country"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Add away competitor"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Add competetion"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Add stage"
+                />
+              </div>
+            </div>
+            <div class="mt-2">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                <input
+                  type="text"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  placeholder="Add stadium"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-6 flex items-center gap-x-6">
+      <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+      <button
+        type="submit"
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Save
+      </button>
+    </div>
+  </form>
 </template>
