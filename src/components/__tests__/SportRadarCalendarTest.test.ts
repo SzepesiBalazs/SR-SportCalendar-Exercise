002224d044e3eb2b4416321d91a09ab6e4acf736
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2024-11-29 11:16:06 +0100

    Adds tests for EventCalendar

--- src/components/__tests__/SportRadarCalendarTest.test.ts
+++ src/components/__tests__/SportRadarCalendarTest.test.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest'
 import AddEvent from '../../views/AddEvent.vue'
 import { mount } from '@vue/test-utils'
 import EventDetails from '../../views/EventDetails'
+import EventCalendar from '../../views/EventCalendar.vue'
 
 describe('AddEvent.vue', () => {
   it('new added to the data on submit', async () => {
@@ -28,5 +29,30 @@ describe('AddEvent.vue', () => {
     const afterCancelData = new EventDetails()
 
     expect(beforeCancelData).not.toBe(afterCancelData)
+  })
+})
+
+describe('EventCalendar.vue', () => {
+  it('tests the first daysarray element has proper keys', () => {
+    const EventCalendarVueTemplate = mount(EventCalendar)
+
+    expect(EventCalendarVueTemplate.vm.daysArray[0]).toHaveProperty('day')
+    expect(EventCalendarVueTemplate.vm.daysArray[0]).toHaveProperty('date')
+    expect(EventCalendarVueTemplate.vm.daysArray[0]).toHaveProperty('events')
+    expect(EventCalendarVueTemplate.vm.daysArray[0]).toHaveProperty('nameOfDay')
+  })
+
+  it('should return unknown if team is not defined', () => {
+    const EventCalendarVueTemplate = mount(EventCalendar)
+
+    expect(EventCalendarVueTemplate.vm.validateCompetitor()).toBe('Unknown')
+    expect(EventCalendarVueTemplate.vm.validateCompetitor('team')).not.toBe('Unknown')
+  })
+
+  it('should return correct time format HH:mm', () => {
+    const EventCalendarVueTemplate = mount(EventCalendar)
+
+    expect(EventCalendarVueTemplate.vm.formatingUTCTime('14:00:00')).toBe('14:00')
+    expect(EventCalendarVueTemplate.vm.formatingUTCTime('15:00')).toBe('15:00')
   })
 })
