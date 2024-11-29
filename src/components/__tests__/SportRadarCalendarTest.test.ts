208d14eff58ef12778c7cbdcfe348614b59779bf
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2024-11-29 10:22:43 +0100

    Adds test for reseting to default with cancel

--- src/components/__tests__/SportRadarCalendarTest.test.ts
+++ src/components/__tests__/SportRadarCalendarTest.test.ts
@@ -1,18 +1,32 @@
 import { describe, it, expect } from 'vitest'
-import AddEvent from "../../views/AddEvent.vue";
+import AddEvent from '../../views/AddEvent.vue'
 import { mount } from '@vue/test-utils'
-import EventDetails from '../../views/EventDetails';
+import EventDetails from '../../views/EventDetails'
 
-describe('adds new event', () => {
-  it('renders properly', async() => {
-    const addEventVueTemplate = mount(AddEvent);
+describe('AddEvent.vue', () => {
+  it('new added to the data on submit', async () => {
+    const addEventVueTemplate = mount(AddEvent)
     const oldLengthOfEventData = addEventVueTemplate.vm.data.length
 
-    const submitButton = addEventVueTemplate.find('button[type="submit"]'); 
-    await submitButton.trigger('click');
-   
+    const submitButton = addEventVueTemplate.find('button[type="submit"]')
+    await submitButton.trigger('click')
+
     const newLengthOfEventData = addEventVueTemplate.vm.data.length
 
-    expect(oldLengthOfEventData + 1).toEqual(newLengthOfEventData);
+    expect(oldLengthOfEventData + 1).toEqual(newLengthOfEventData)
+  })
+
+  it('reset default on cancel', async () => {
+    const addEventVueTemplate = mount(AddEvent)
+
+    addEventVueTemplate.vm.eventDetail = new EventDetails('2024-11-01')
+    const beforeCancelData = addEventVueTemplate.vm.eventDetail
+
+    const cancelButton = addEventVueTemplate.find('button[type="button"]')
+    await cancelButton.trigger('click')
+
+    const afterCancelData = new EventDetails()
+
+    expect(beforeCancelData).not.toBe(afterCancelData)
   })
 })
